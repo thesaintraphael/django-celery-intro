1bc2932b6586740ebedccf2442ccc53bba56112f
Rafael Salimov <raphasalimov@gmail.com>
2022-02-18 23:48:49 +0400

    dockerized app

--- tasks/tests/test_views.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse
-
-
-def test_home(client):
-    url = reverse("home")
-    response = client.get(url)
-    assert response.status_code == 200
--- tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("tasks/<task_id>/", views.get_status, name="get_status"),
-    path("tasks/", views.run_task, name="run_task"),
-    path("", views.home, name="home"),
-]
